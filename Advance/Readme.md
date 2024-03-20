25db393fc6cb82db0a5ca99ea84ff51996452bde
Ajay <ajaysonkarp@gmail.com>
2024-03-20 11:15:29 +0530

    Cookies and Session

--- Advance/Readme.md
+++ Advance/Readme.md
@@ -1,3 +1,90 @@
 ### Advance Topics
 - Install MongoDB
-- Cookies(Client) and Session(server)
+- Cookies and Session
+ **Session (Server)**
+```
+# npm i express-session
+```
+- app.js, 
+```
+
+var session = require('express-session')
+
+app.use(session({
+  resave : false,               // save only new session 
+  saveUninitialized : false,
+  secret: "encription algorithm"
+}));
+```
+**whenever server will restart, sessions will be cleared**
+
+- index.js
+```
+//creating sessions
+
+router.get("/session" , function(req , res){
+  req.session.ban = true;    
+  <!-- ban is just example we can give name anithing
+   to the session -->
+  res.render("index");
+})
+```
+```
+//checking/Read the session
+
+router.get("/checksession", function(req, res){
+  if (req.session.ban == true){
+    res.send("You are banned");
+  }
+  else{
+    res.send("You are active !!")
+  }
+})
+```
+```
+//delete session
+
+router.get("/deletesession", function(req, res){
+  req.session.destroy(function(err){
+    if (err) throw err;
+    res.send("Ban removed !!!")
+  })
+})
+```
+**Cookies (Client)**
+
+- app.js
+```
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+```
+
+- index.js
+```
+//creating cookie
+
+router.get("/cookies", function(req, res){
+  <!--res.cookie("name", value)-->
+  res.cookie("age",25);   
+  res.send("Cookie created !!");
+})
+```
+```
+//Read the cookie
+
+router.get("/readcookies",function(req, res){
+  <!-- req.cookies.name -->
+  var coo = req.cookies.age;
+  res.send("cookie is "+ coo);
+})
+```
+```
+//Delete the cookie
+
+router.get("/deletecookies", function(req, res){
+  <!-- res.clearCookie("name") -->
+  res.clearCookie("age");
+  res.send("All cookies cleared !!")
+})
+
+```
--- Advance/routes/index.js
+++ Advance/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const userModel = require('./users');
 
-router.get('/', function(req, res){
-  res.render("index")
-})
+// router.get('/', function(req, res){
+//   res.render("index")
+// })
 
 router.get('/contact', function(req, res){
   res.render("contact")
@@ -17,14 +17,14 @@ router.get('/contact', function(req, res){
 // //   but i want to execute function first so that 
 // //   i am using "awiat" and and awiat is come always with "async"
 
-// router.get('/create' ,async function(req, res){
-//   const createduser = await userModel.create({
-//     username : "moturam",
-//     age : 27,
-//     name : "motu"
-//   });
-//   res.send(createduser);
-// });
+router.get('/create' ,async function(req, res){
+  const createduser = await userModel.create({
+    username : "moturam",
+    age : 27,
+    name : "motu"
+  });
+  res.send(createduser);
+});
 //--------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------
@@ -37,10 +37,10 @@ router.get("/find" , async function(req , res){
 
 //--------------------------------------------------------------------------
 //show only one document
-// router.get("/find" , async function(req , res){
-//   const alluser = await userModel.findOne({name: "chhotu"});
-//   res.send(alluser);
-// })
+router.get("/find" , async function(req , res){
+  const alluser = await userModel.findOne({name: "chhotu"});
+  res.send(alluser);
+})
 //--------------------------------------------------------------------------
 
 
@@ -48,11 +48,64 @@ router.get("/find" , async function(req , res){
 //--------------------------------------------------------------------------
 //Delete one document from collection
 
-// router.get('/deleteone', async function(req, res ){
-//  const deleteduser =  await userModel.findOneAndDelete({
-//     name: "chhotu"
-//   })
-//   res.send(deleteduser);
-// })
+router.get('/deleteone', async function(req, res ){
+ const deleteduser =  await userModel.findOneAndDelete({
+    name: "chhotu"
+  })
+  res.send(deleteduser);
+})
 //--------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------
+//Session
+//creating sessions
+
+router.get("/session" , function(req , res){
+  req.session.ban = true;
+  res.render("index");
+})
+
+//checking the session
+router.get("/checksession", function(req, res){
+  if (req.session.ban == true){
+    res.send("You are banned");
+  }
+  else{
+    res.send("You are active !!")
+  }
+})
+
+//delete session
+router.get("/deletesession", function(req, res){
+  req.session.destroy(function(err){
+    if (err) throw err;
+    res.send("Ban removed !!!")
+  })
+})
+//--------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------
+//Cookies
+//creating cookie
+router.get("/cookies", function(req, res){
+  res.cookie("age",25);
+  res.send("Cookie created !!");
+})
+
+//Read the cookie
+router.get("/readcookies",function(req, res){
+  var coo = req.cookies.age;
+  res.send("cookie is "+ coo);
+})
+
+router.get("/deletecookies", function(req, res){
+  res.clearCookie("age");
+  res.send("All cookies cleared !!")
+})
+//--------------------------------------------------------------------------
+
+
 module.exports = router;
